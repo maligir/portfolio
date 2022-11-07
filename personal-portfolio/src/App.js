84c13506ef1f48077daa703e9b26996667bd725b
Rahul Maligi <maligir@utexas.edu>
2022-11-07 10:19:14 -0600

    changed favicon and title of app

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -7,10 +7,17 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Rahul's Portfolio</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="This is a personal portfolio website built using React.js" />
+      </Helmet>
       <NavBar />
       <Banner />
       <Skills />
